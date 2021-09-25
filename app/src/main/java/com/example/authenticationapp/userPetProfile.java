c1a44b35b1ddf42c43372b7785318a15fbfdc327
senarathnaPP <IT20204020@my.sliit.lk>
2021-09-26 01:43:54 +0530

    authentication app v2

--- app/src/main/java/com/example/authenticationapp/userPetProfile.java
+++ app/src/main/java/com/example/authenticationapp/userPetProfile.java
@@ -23,10 +23,7 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 public class userPetProfile extends AppCompatActivity {
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/main
     ImageButton imageButton;
     EditText description,category,telephonenumber,price;
     Button btnaddcourse, btnshowcourse,btn_backhome;
